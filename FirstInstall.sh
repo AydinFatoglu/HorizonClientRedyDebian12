aafba21d95f65ea31d083cfa9130f1a2aa25be24
AydinFatoglu <86136084+AydinFatoglu@users.noreply.github.com>
2024-09-29 21:55:15 +0300

    Update FirstInstall.sh

--- FirstInstall.sh
+++ FirstInstall.sh
@@ -37,7 +37,7 @@ yes yes | ./VMware-Horizon-Client-2209-8.7.0-20616018.x64.bundle --eulas-agreed 
 --set-setting vmware-horizon scanner true
 
 apt install gcc-12 g++-12 -y
-apt install python3-tk
+apt install python3-tk -y
 
 
 # Define the username for auto-login
@@ -93,6 +93,70 @@ EOF
 chown -R $USERNAME:$USERNAME /home/$USERNAME/.config/openbox
 
 echo "Openbox autostart file created for $USERNAME at /home/$USERNAME/.config/openbox/autostart"
+
+#!/bin/bash
+
+# Define the username for auto-login
+USERNAME="examuser"
+
+# Create the necessary directories in the user's home path
+mkdir -p /home/$USERNAME/.config/openbox
+
+# Set the path for the Openbox menu.xml file
+MENU_FILE="/home/$USERNAME/.config/openbox/menu.xml"
+
+# Write the XML content into the menu.xml file
+cat << EOF > "$MENU_FILE"
+<?xml version="1.0" encoding="UTF-8"?>
+
+<openbox_menu xmlns="http://openbox.org/"
+        xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+        xsi:schemaLocation="http://openbox.org/
+                file:///usr/share/openbox/menu.xsd">
+
+<menu id="root-menu" label="Openbox 3">
+  <item label="Terminal emulator">
+    <action name="Execute"><execute>x-terminal-emulator</execute></action>
+  </item>
+  <item label="Web browser">
+    <action name="Execute"><execute>x-www-browser</execute></action>
+  </item>
+  <!-- This requires the presence of the 'obamenu' package to work -->
+  <menu id="/Debian" />
+  <separator />
+  <menu id="applications-menu" label="Applications" execute="/usr/bin/obamenu"/>
+  <separator />
+  
+  <!-- Add your custom item here -->
+  <item label="Autostart File Editor">
+    <action name="Execute"><execute>python3 /home/$USERNAME/autostart.editor.py</execute></action>
+  </item>
+  
+  <separator />
+  <item label="ObConf">
+    <action name="Execute"><execute>obconf</execute></action>
+  </item>
+  <item label="Reconfigure">
+    <action name="Reconfigure" />
+  </item>
+  <item label="Restart">
+    <action name="Restart" />
+  </item>
+  <separator />
+  <item label="Exit">
+    <action name="Exit" />
+  </item>
+</menu>
+
+</openbox_menu>
+EOF
+
+# Set the correct ownership for the user
+chown -R "$USERNAME:$USERNAME" /home/$USERNAME/.config/openbox
+
+echo "menu.xml file has been created for $USERNAME at $MENU_FILE"
+
+
 
 
 
